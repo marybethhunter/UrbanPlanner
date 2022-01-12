50d1298f1bc2b282196230007c0869d8af433f1b
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-01-11 20:09:53 -0600

    Add project files.

---
+++ Urban Planner/Building.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urban_Planner
+{
+    class Building
+    {
+        //constructor
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        //fields
+        private string _designer = "Mary Beth Hunter";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        //public properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        private double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        //public methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+        }
+
+        public void GiveDetails(string building)
+        {
+            Console.WriteLine($"{_address}------------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+        }
+    }
+}
---
+++ Urban Planner/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Urban_Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEight = new Building("512 8th Avenue");
+            Building TwoSixFourNine = new Building("264 9th Avenue");
+            Building ThreeSevenTwoOne = new Building("372 1st Avenue");
+            Building FourFourFourThree = new Building("444 3rd Avenue");
+
+            FiveOneTwoEight.Width = 20;
+            FiveOneTwoEight.Depth = 40;
+            FiveOneTwoEight.Stories = 4;
+
+            TwoSixFourNine.Width = 10;
+            TwoSixFourNine.Depth = 50;
+            TwoSixFourNine.Stories = 2;
+
+            ThreeSevenTwoOne.Width = 50;
+            ThreeSevenTwoOne.Depth = 100;
+            ThreeSevenTwoOne.Stories = 10;
+
+            FourFourFourThree.Width = 100;
+            FourFourFourThree.Depth = 200;
+            FourFourFourThree.Stories = 20;
+
+            FiveOneTwoEight.Construct();
+            TwoSixFourNine.Construct();
+            ThreeSevenTwoOne.Construct();
+            FourFourFourThree.Construct();
+
+            FiveOneTwoEight.Purchase("Mary Beth Hunter");
+            TwoSixFourNine.Purchase("Skye Duff");
+            ThreeSevenTwoOne.Purchase("Lauren Hignite");
+            FourFourFourThree.Purchase("Jennifer Meadows");
+
+            FiveOneTwoEight.GiveDetails("512 8th Avenue");
+            Console.WriteLine(" ");
+            TwoSixFourNine.GiveDetails("264 9th Avenue");
+            Console.WriteLine(" ");
+            ThreeSevenTwoOne.GiveDetails("372 1st Avenue");
+            Console.WriteLine(" ");
+            FourFourFourThree.GiveDetails("444 3rd Avenue");
+            Console.WriteLine(" ");
+        }
+    }
+}
